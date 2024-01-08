6f1d4cd33d9a3df473fff011e3895508fc5d363a
Quaresm <matheusssquaresma@gmail.com>
2024-01-08 18:50:29 -0300

    adding models

--- pvd_app/__init__.py
+++ pvd_app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+migrate = Migrate(app, db)
 
 dotenv_path = os.path.join(os.path.dirname(__file__), 'secret', '.env')
 load_dotenv(dotenv_path)
@@ -12,7 +13,9 @@ load_dotenv(dotenv_path)
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['SQLACHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+    db.init_app(app)
+    
 
     from .views import views
     from .auth import auth
--- pvd_app/main.py
+++ pvd_app/main.py
@@ -1,4 +1,4 @@
-from app import create_app
+from pvd_app import create_app
 
 app = create_app()
 
